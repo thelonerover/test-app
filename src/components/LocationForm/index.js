73375c4af3fe70492a77fcb1f727899ade37d3b7
Vladimir Komarov <34229011+thelonerover@users.noreply.github.com>
2020-10-15 18:59:57 +0300

    simple validation

--- src/components/LocationForm/index.js
+++ src/components/LocationForm/index.js
@@ -1,12 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import NumericInput from 'react-numeric-input';
 
 export default () => {
-    const [metrics, setMetrics] = useState('dec');
-    const [coordinates, seCoordinates] = useState({lat: '', lon: ''});
+    const [decimalValue, setDecimalValue] = useState(true);
+    const [coordinates, setCoordinates] = useState({lat: 0, lon: 0});
 
-    const handleChange = type => e => {
+    const handleRadio = value => e => {
+        setDecimalValue(value);
+    }
+
+    const handleBlur = e => {
         e.preventDefault();
-        seCoordinates({...coordinates, [type]: e.target.value});
+        e.target.value = +e.target.value;
+    }
+
+    const handleChange = (type, boundary) => e => {
+        e.preventDefault();
+        const regex = /^[-+]?\d*?(\.\d{0,7})?$/;
+        let value = e.target.value;
+
+        console.log(value, value.match(regex));
+
+        if (value.match(regex)) {
+            setCoordinates({...coordinates, [type]: value});
+
+            if (value < -boundary) {
+                setCoordinates({...coordinates, [type]: -boundary});
+            } else if (value > boundary) {
+                setCoordinates({...coordinates, [type]: boundary});
+            }
+        } else if (!value) {
+            setCoordinates({...coordinates, [type]: 0});
+        } 
     }
 
     return (
@@ -19,19 +44,26 @@ export default () => {
                 <div>
                     <span>Metrics</span>
                     <div>
-                        <button>Decimal</button>
+                        <label>
+                            Decimal
+                            <input type='radio' name='metrics' value='Decimal' checked={decimalValue} onClick={handleRadio(true)} />
+                        </label>
                         <span>/</span>
-                        <button>DMS</button>
+                        <label>
+                            DMS
+                            <input type='radio' name='metrics' value='Decimal' checked={!decimalValue} onClick={handleRadio(false)} />
+                        </label>
                     </div>
                 </div>
                 <div>
                     <label>
                         <span>Latitude</span>
-                        <input name='lat' type='text' onChange={handleChange('lat')} value={coordinates.lat}/>
+                        <input type='text' placeholder='00.0000000' value={coordinates.lat} onBlur={handleBlur} onChange={handleChange('lat', 90)} />
+                        {/* <NumericInput min={-90} max={90} value={0} precision={7}   /> */}
                     </label>
                     <label>
                         <span>Longitude</span>
-                        <input name='lon' type='text' onChange={handleChange('lon')} value={coordinates.lon}/>
+                        <NumericInput min={-180} max={180} value={0} maxLength={11}  />
                     </label>
                 </div>
                 <hr />
